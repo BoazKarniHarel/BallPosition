12ade1b4a7b78adc88c3ff4a14b26bd88c7974db
BoazKarniHarel <boazkarni@gmail.com>
2024-07-08 09:28:53 +0300

    feat: save clips
    .

---
+++ common/constants.py
@@ -0,0 +1,11 @@
+import os
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
+DATA_DIR = os.path.join(PROJECT_ROOT, "data")
+# url -> [start download time, end download time]
+# TODO verify if both sports should be use or just one.
+CLIPS_URLS_DICT = {
+    "https://www.youtube.com/watch?v=L0HTZ0XBv_o&ab_channel=Wimbledon": [15, 65],
+    "https://www.youtube.com/watch?v=OthPga0vEkE&ab_channel=Wimbledon": [15, 65],
+    "https://www.youtube.com/watch?v=XVrOXUAsqmQ&ab_channel=Wimbledon": [15, 65],
+}
